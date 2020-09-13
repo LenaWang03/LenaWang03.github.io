84444b842d7bf8068f7418cd494558c4186b46b9
LenaWang03 <58739287+LenaWang03@users.noreply.github.com>
2020-09-13 19:27:07 -0400

    image issuessssss

--- pages/views.py
+++ pages/views.py
@@ -4,6 +4,8 @@ from django.views.generic import TemplateView
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
-
 class AboutPageView(TemplateView): 
     template_name = 'about.html'
+
+class PaintingsPageView(TemplateView): 
+    template_name = 'paintings.html'
